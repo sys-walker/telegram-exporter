4f2e555d2b7f23a183f71de4f89099135f5b07a0
Pere Antoni <56586435+sys-walker@users.noreply.github.com>
2021-10-17 14:49:53 +0200

    Update contacts-exporter.py

--- contacts-exporter.py
+++ contacts-exporter.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python
-from telethon.sync import TelegramClient
-from telethon import functions
-import phonenumbers, re
 from datetime import datetime
+
+import phonenumbers
+import re
+from telethon import functions
+from telethon.sync import TelegramClient
 
 # To get API_ID and API_HASH
 # https://my.telegram.org/auth?to=apps
 # check 'App configuration' section
 
-API_ID=000000 # Your API_ID
-API_HASH="YOUR API_HASH"
-
-client = TelegramClient('Contacts_Exporter', api_id=API_ID, api_hash=API_HASH)
+API_ID = 000000  # Your API_ID
+API_HASH = "#########_YOUR_API_HASH_########"
 now_str = f"{datetime.now()}".replace(" ", "_").replace(":", ".")
 
 
-async def start():
-    await client.get_me()
-
-
-def generate_card(param):
-    card = VCARD()
-    card.set_full_name(f"{param['first_name']} {param['last_name']}")
-    card.set_nickname(f"{param['username']}")
-    card.set_phone(f"{param['phone']}")
-    card.build()
-    return card
-
-
 class VCARD:
+    """
+    Card generator for Telegram contacts using the builder pattern
+    """
+
     def __init__(self):
         self.__header = "BEGIN:VCARD\n" \
                         "VERSION:3.0\n"
@@ -36,15 +27,14 @@ class VCARD:
         self.__footer = "END:VCARD"
 
         self.__card = ""
+        self.__has_phone = False
 
-        pass
-
-    def set_full_name(self, name):
-        name = name.replace("None", "")
-        self.__body_card += f"FN:{name}\n"
+    def set_full_name(self, full_name):
+        full_name = full_name.replace("None", "")
+        self.__body_card += f"FN:{full_name}\n"
 
         self.__body_card += "N:"
-        lst = name.split(" ")
+        lst = full_name.split(" ")
         for i in range(0, 4):
             if i < len(lst) and f"{lst[i]}" != '':
                 self.__body_card += f"{lst[i]}"
@@ -58,11 +48,12 @@ class VCARD:
             self.__body_card += f"NICKNAME:{nickname}\n"
         return self
 
-    def set_phone(self, name):
-        if name != "" and name != "None":
-            formated_phone = self.__format_number(name)
+    def set_phone(self, phone):
+        if phone != "" and phone != "None":
+            formated_phone = self.__format_number(phone)
             if formated_phone != "":
                 self.__body_card += f"TEL;TYPE=CELL:{formated_phone}\n"
+                self.__has_phone = True
         return self
 
     def build(self):
@@ -71,17 +62,14 @@ class VCARD:
                       self.__footer
         return self
 
-    def __str__(self):
-        return self.__card
+    def __format_number(self, input_number):
 
-    def __format_number(self, InputNumber):
-
-        format = re.sub("(?:\+)?(?:[^[0-9]*)", "", InputNumber)
-        FormattedPhoneNumber = "+" + format
+        format_num = re.sub("(?:\+)?(?:[^[0-9]*)", "", input_number)
+        FormattedPhoneNumber = "+" + format_num
 
         try:
             PhoneNumberObject = phonenumbers.parse(FormattedPhoneNumber, None)
-        except Exception as e:
+        except Exception:
             return ""
         else:
             if not phonenumbers.is_valid_number(PhoneNumberObject):
@@ -91,30 +79,86 @@ class VCARD:
                 PhoneNumberObject, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
             return internationalNumber
 
+    def has_phone(self) -> bool:
+        return self.__has_phone
+
+    def __str__(self) -> str:
+        return self.__card
+
+
+class TelegramConnection:
+    """
+        custom TelegramClient with task to get the contact list in telegem
+    """
+    def __init__(self):
+        self.__client = TelegramClient('Contacts_Exporter', api_id=API_ID, api_hash=API_HASH)
+        pass
+
+    def connect(self):
+        self.__client.start()
+        self.__client.loop.run_until_complete(self.__start())
+
+    def disconnect(self):
+        self.__client.log_out()
+        print("Successful Logout")
+
+    async def __start(self):
+        await self.__client.get_me()
+
+    def get_contacts(self) -> list:
+        return self.__client(functions.contacts.GetContactsRequest(hash=0)).__dict__['users']
+
+
+def generate_card(contact_dict):
+    card = VCARD()
+    card.set_full_name(f"{contact_dict['first_name']} {contact_dict['last_name']}")
+    card.set_nickname(f"{contact_dict['username']}")
+    card.set_phone(f"{contact_dict['phone']}")
+    card.build()
+    return card
+
+
+def save_to_file(_filename_, ignore_mode_, contact_objects):
+    ignored = 0
+    count = 0
+    with open(_filename_, 'w') as f:
+        for contact in contact_objects:
+            card = generate_card(contact.__dict__)
+            if ignore_mode_:
+                if card.has_phone():
+                    print(card, file=f)
+                    count += 1
+                else:
+                    ignored += 1
+            else:
+                print(card, file=f)
+                count += 1
+    print(f"Exported {count}")
+    print(f"Ignored {ignored}")
+    print(f"Saved into {_filename_}")
+
 
 if __name__ == '__main__':
-
-    client.start()
-    client.loop.run_until_complete(start())
-    result = client(functions.contacts.GetContactsRequest(
-        hash=0
-    )).__dict__['users']
+    client = TelegramConnection()
+    client.connect()
+    result = client.get_contacts()
     print(f"You have {len(result)} contacts in telegram")
-    while (True):
-        cmd = input("Start? [Y/N]: ")
-        if cmd.lower() == "y":
-            with open(f'TelegramExport{now_str}.vcf', 'w') as f:
-                for ae in result:
-                    item = ae.__dict__
-                    print(generate_card(item), file=f)
-            print(f"Saved into TelegramExport{now_str}.vcf")
-            client.log_out()
-            print("Successful Logout")
-            break
-        elif cmd.lower() == "n":
+    while True:
+        export = input("Export contacts? [Y/N]: ")
+        if export.lower() == "y":
+            ignoring = input("ignore contacts without phone? [Y/N]: ")
+            if not (ignoring.lower() == "y" or ignoring.lower() == "n"):
+                print("Invalid option")
+                continue
+            else:
+                ignore_mode = True if ignoring.lower() == "y" else False
+                filename = f"TCExport_no_phone_ignrd_{now_str}.vcf" if ignore_mode else f"TCExport_{now_str}.vcf"
+                save_to_file(filename, ignore_mode, result)
+                client.disconnect()
+                break
+        elif export.lower() == "n":
             print("Nothing was export")
-            client.log_out()
-            print("Successful Logout")
+            client.disconnect()
             break
         else:
-            pass
+            continue
